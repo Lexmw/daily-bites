cd836d42584a8593a778bfe89c8df1b3f6445c5a
Lexius <lexius.waltar@gmail.com>
2023-06-29 12:47:30 -0400

    adds the compare arrays function that will compare the incoming category data with the currently displayed data and show the new recipes.

--- src/views/searchResults.jsx
+++ src/views/searchResults.jsx
@@ -23,7 +23,7 @@ export default function SearchResults() {
     //inital page load fetch of data
     const dataFetch = async () => {
       const data = await (
-        await fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=s")
+        await fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
       ).json();
 
       setResults(data?.meals);
@@ -33,6 +33,28 @@ export default function SearchResults() {
     dataFetch();
   }, []);
 
+  const newCategorySearchResults = (array1, array2) => {
+    console.log(array1);
+    console.log(array2)
+    var differentValues = [];
+
+    for (var i = 0; i < array1.length; i++) {
+      if (!array2.some(x => x.idMeal === array1[i].idMeal)) {
+        differentValues.push(array1[i]);
+      }
+    }
+
+    for (var j = 0; j < array2.length; j++) {
+      if (!array1.some(v => v.idMeal === array2[j].idMeal)) {
+        differentValues.push(array2[j]);
+      }
+    }
+
+    console.log(differentValues);
+    return differentValues;
+  };
+
+
   const searchCategoryRecipes = async (e) => {
     setIsLoaded(false);
     const data = await (
@@ -41,67 +63,48 @@ export default function SearchResults() {
       )
     ).json();
 
-    //check if the meal is already in the list if it isnt add it. If it is add it to the front of the list
-    //setResults(curr => curr.filter(x => data.meals.some(value => curr.includes(value))));
-    setResults((curr) => {
-      let diffRecipes = data.meals.filter((x) => curr.indexOf(x) > 0);
-      console.log(
-        data.meals.indexOf[
-          {
-            idMeal: "52965",
-            strMeal: "Breakfast Potatoes",
-            strMealThumb:
-              "https://www.themealdb.com/images/media/meals/1550441882.jpg"
-          }
-        ],
-        curr[2],
-        data.meals
-      );
-      console.log("diff", diffRecipes);
-      return [...curr];
-    });
-    setIsLoaded(true);
+    setResults((curr) => newCategorySearchResults(data.meals, curr));
 
-    console.log(results);
+    setIsLoaded(true);
   };
 
   return (
     <>
-    <Container id="search-results" pt="50px"maxW="100%">
-      <Flex direction="row" align="flex-start" m="20">
-        <Container
-          w="20%"
-          p="10"
-          align="left"
-          minW="180px"
-          bg="grey"
-          color="white"
-          rounded="md"
-        >
-          <CategoryCheckboxes searchCategoryRecipes={searchCategoryRecipes} />
-        </Container>
-        <Container
-          w="75%"
-          maxW="100%"
-          border="1px"
-          borderColor="gray.200"
-          rounded="md"
-        >
-          {results.map((recipe, index) => (
-            <Skeleton key={recipe.idMeal} height="100%" isLoaded={isLoaded}>
-              <Card
-                dishName={recipe.strMeal}
-                img={recipe.strMealThumb}
-                area={recipe?.strArea}
-                category={recipe?.strCategory}
-                id={recipe.idMeal}
-                key={recipe.idMeal}
-              />
-            </Skeleton>
-          ))}
-        </Container>
-      </Flex>
-    </Container>
+      <Container id="search-results" pt="50px" maxW="100%">
+        <Flex direction="row" align="flex-start" m="20">
+          <Container
+            w="20%"
+            p="10"
+            align="left"
+            minW="180px"
+            bg="grey"
+            color="white"
+            rounded="md"
+          >
+            <CategoryCheckboxes searchCategoryRecipes={searchCategoryRecipes} />
+          </Container>
+          <Container
+            w="75%"
+            maxW="100%"
+            border="1px"
+            borderColor="gray.200"
+            rounded="md"
+          >
+            {results.map((recipe, index) => (
+              <Skeleton key={recipe.idMeal} height="100%" isLoaded={isLoaded}>
+                <Card
+                  dishName={recipe.strMeal}
+                  img={recipe.strMealThumb}
+                  area={recipe?.strArea}
+                  category={recipe?.strCategory}
+                  id={recipe.idMeal}
+                  key={recipe.idMeal}
+                />
+              </Skeleton>
+            ))}
+          </Container>
+        </Flex>
+      </Container>
     </>
   );
 }
